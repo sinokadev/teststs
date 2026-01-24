6dc5dff5dcec0b25592700eab3901ac249bb4993
Kanowara Sinoka <maainnewkin59@gmail.com>
2026-01-25 05:17:50 +0900

    Enhance run_test and testtest functions with equality check

--- teststs.py
+++ teststs.py
@@ -1,18 +1,20 @@
-def run_test(tests, func):
-    for value, expected in tests:
-        result = func(value)
-        if result != expected:
-            return False, value, expected, result
+def run_test(tests, func, eq=lambda a, b: a == b):
+    for case in tests:
+        *args, expected = case
+        result = func(*args)
+        if not eq(result, expected):
+            return False, args, expected, result
     return True, None, None, None
 
-def testtest(tests, func, detail = False):
-    result = run_test(tests, func)
-    if detail:
-        if not result[0]: 
-            print("Your code is shit\n", "input:", result[1], ", expected:", result[2], ", actual", result[3])
-            return
-    else:
-        if not result[0]: 
-            print("Your code is shit", result[1], result[3])
-            return
+def testtest(tests, func, detail=False, eq=lambda a, b: a == b):
+    ok, args, expected, result = run_test(tests, func, eq)
+    if not ok:
+        if detail:
+            print("Your code is shit")
+            print("input:", args)
+            print("expected:", expected)
+            print("actual:", result)
+        else:
+            print("Your code is shit", args, result)
+        return
     print("OK!")
