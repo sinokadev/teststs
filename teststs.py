a9c20567bbc1b09dce828357862c261f547a282a
Kanowara Sinoka <maainnewkin59@gmail.com>
2026-01-25 05:18:40 +0900

    Rename run_test to _run_test and testtest to teststs

--- teststs.py
+++ teststs.py
@@ -1,4 +1,4 @@
-def run_test(tests, func, eq=lambda a, b: a == b):
+def _run_test(tests, func, eq=lambda a, b: a == b):
     for case in tests:
         *args, expected = case
         result = func(*args)
@@ -6,8 +6,8 @@ def run_test(tests, func, eq=lambda a, b: a == b):
             return False, args, expected, result
     return True, None, None, None
 
-def testtest(tests, func, detail=False, eq=lambda a, b: a == b):
-    ok, args, expected, result = run_test(tests, func, eq)
+def teststs(tests, func, detail=False, eq=lambda a, b: a == b):
+    ok, args, expected, result = _run_test(tests, func, eq)
     if not ok:
         if detail:
             print("Your code is shit")
