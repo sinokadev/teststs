295c67304f5958930101a0c3eea911bf0812b3c0
Kanowara Sinoka <maainnewkin59@gmail.com>
2026-01-24 20:32:55 +0000

    add except

--- teststs.py
+++ teststs.py
@@ -1,19 +1,25 @@
-def _run_test(tests, func, eq=lambda a, b: a == b):
+def _run_test(tests, func, eq=lambda a, b: a == b):    
     for case in tests:
         *args, expected = case
-        result = func(*args)
+        try:
+            result = func(*args)
+        except Exception as error:
+            return False, args, expected, result, error
         if not eq(result, expected):
-            return False, args, expected, result
-    return True, None, None, None
+            return False, args, expected, result, None
+
+    return True, None, None, None, None
 
 def teststs(tests, func, detail=False, eq=lambda a, b: a == b):
-    ok, args, expected, result = _run_test(tests, func, eq)
+    ok, args, expected, result, error = _run_test(tests, func, eq)
     if not ok:
         if detail:
             print("Your code is shit")
             print("input:", args)
             print("expected:", expected)
             print("actual:", result)
+            if error:
+                print("error:", error)
         else:
             print("Your code is shit", args, result)
         return
