aafb3ee3fb3faf83ba8bf1cbd7f311e08d2e6ebd
sinokadev <sinoka@sinoka.dev>
2026-01-25 06:05:11 +0900

    action

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="teststs",
+    version="{{VERSION_PLACEHOLDER}}",
+    author="Kanowara Sinoka",
+    author_email="sinok@sinoka.dev",
+    description="A Simple, Tiny Test Library for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sinokadev/teststs",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 3-Clause License",
+        "Operating System :: OS Independent",
+    ],
+)
